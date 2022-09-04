24b9d82ccdddecf530e9ed396d2508e012a4f49d
Ferrerov <valenferrero01@gmail.com>
2022-09-04 10:40:18 -0300

    Clases y metodos estaticos - Ejercicios 02

---
+++ Clases y metodos estaticos/I02_DeseaContinuar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace I02_DeseaContinuar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int sumaTotal = 0;
+
+            Console.Write("Ingrese un primer numero: ");
+            while (!int.TryParse(Console.ReadLine(), out numeroIngresado))
+            {
+                Console.Write("Error, ingrese un primer numero valido: ");
+            }
+            sumaTotal += numeroIngresado;
+            
+            do
+            {
+                Console.Write("Ingrese un numero a sumar: ");
+                while (!int.TryParse(Console.ReadLine(), out numeroIngresado))
+                {
+                    Console.Write("Error, ingrese un numero valido: ");
+                }
+                sumaTotal += numeroIngresado;
+                Console.Clear();
+                Console.WriteLine($"Suma total: {sumaTotal}");
+
+                Console.Write("¿Desea continuar? (S/N): ");
+            } while (Validador.ValidarRespuesta(Console.ReadLine()));
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ Clases y metodos estaticos/I02_DeseaContinuar/Validador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I02_DeseaContinuar
+{
+    internal class Validador
+    {
+        public static bool ValidarRespuesta(string respuesta)
+        {
+            if(respuesta.ToLower() == "s")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
